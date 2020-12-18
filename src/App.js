761cc29c684feb8de21d7f45130586d7f997a0f5
frasnym <frastyawan.nym@gmail.com>
2020-12-18 09:05:10 +0800

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import HomePage from "./containers/HomePage/HomePage";
+
+import "./assets/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route path="/" component={HomePage} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
